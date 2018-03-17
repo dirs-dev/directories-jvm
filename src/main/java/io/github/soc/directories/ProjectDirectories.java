2986e5ad9c5f21c7fc85456323ceea238d2c3851
soc <simon@ochsenreither.de>
2018-03-17 05:22:49 +0100

    Directories 7
    - Split `BaseDirectories` into `BaseDirectories` (cache, config, ...)
      and `UserDirectories` (audio, video, ...)
    - Fields of `BaseDirectories` and `UserDirectories` are now public
    - Add substantial amounts of documentation
    - The fields of `UserDirectories` are now `null` if no values can be
      retrieved from `xdg-user-dirs`, instead of falling back on hard-coded
      values
    - Changes to some values of `BaseDirectories` and `ProjectDirectories`

--- src/main/java/io/github/soc/directories/ProjectDirectories.java
+++ src/main/java/io/github/soc/directories/ProjectDirectories.java
@@ -4,6 +4,23 @@ import java.util.Locale;
 
 import static io.github.soc.directories.Util.*;
 
+/** <code>ProjectDirectories</code> computes the location of cache, config or data directories for a specific application,
+  * which are derived from the standard directories and the name of the project/organization.
+  *
+  * <h2>Examples</h2>
+  *
+  * All examples on this page are computed with a user named <em>Alice</em>,
+  * and a <code>ProjectDirectories</code> instance created with
+  * <code>ProjectDirectories.from("com", "Foo Corp", "Bar App")</code>.
+  * <p>
+  * <code style="white-space: pre">
+  * var projectDirs = ProjectDirectories.from("com", "Foo Corp",  "Bar App");
+  * projectDirs.configDir;
+  * // Linux:   /home/alice/.config/barapp
+  * // Windows: C:\Users\Alice\AppData\Roaming\Foo Corp\Bar App
+  * // macOS:   /Users/Alice/Library/Preferences/com.Foo-Corp.Bar-App
+  * </code>
+  */
 public final class ProjectDirectories {
 
   private ProjectDirectories(
@@ -24,15 +41,77 @@ public final class ProjectDirectories {
     this.runtimeDir   = runtimeDir;
   }
 
+  /** Returns the project path fragment used to compute the project's cache/config/data directories.
+    * <p>
+    * The value is derived from the arguments provided to the {@link ProjectDirectories#from} call and is platform-dependent.
+    */
   public final String projectPath;
+  /** Returns the path to the project's cache directory.
+    * <p>
+    * <code style="white-space: pre">
+    * |Platform | Value                                                            | Example                                                |
+    * | ------- | ---------------------------------------------------------------- | ------------------------------------------------------ |
+    * | Linux   | `$XDG_CACHE_HOME_project_path_` or `$HOME/.cache/_project_path_` | /home/alice/.cache/barapp                              |
+    * | macOS   | `$HOME/Library/Caches/_project_path_`                            | /Users/Alice/Library/Caches/com.Foo-Corp.Bar-App/cache |
+    * | Windows | `{FOLDERID_LocalAppData}\_project_path_\cache`                   | C:\Users\Alice\AppData\Local\Foo Corp\Bar App\cache    |
+    * </code>
+    */
   public final String cacheDir;
+  /** Returns the path to the project's config directory.
+    * <p>
+    * <code style="white-space: pre">
+    * |Platform | Value                                                               | Example                                                     |
+    * | ------- | ------------------------------------------------------------------- | ----------------------------------------------------------- |
+    * | Linux   | `$XDG_CONFIG_HOME/_project_path_` or `$HOME/.config/_project_path_` | /home/alice/.config/barapp                                  |
+    * | macOS   | `$HOME/Library/Preferences/_project_path_`                          | /Users/Alice/Library/Preferences/com.Foo-Corp.Bar-App/cache |
+    * | Windows | `{FOLDERID_RoamingAppData}\_project_path_\config`                   | C:\Users\Alice\AppData\Roaming\Foo Corp\Bar App\config      |
+    * </code>
+    */
   public final String configDir;
+  /** Returns the path to the project's data directory.
+    * <p>
+    * <code style="white-space: pre">
+    * |Platform | Value                                                                  | Example                                                            |
+    * | ------- | ---------------------------------------------------------------------- | ------------------------------------------------------------------ |
+    * | Linux   | `$XDG_DATA_HOME/_project_path_` or `$HOME/.local/share/_project_path_` | /home/alice/.local/share/barapp                                    |
+    * | macOS   | `$HOME/Library/Application Support/_project_path_`                     | /Users/Alice/Library/Application Support/com.Foo-Corp.Bar-App/data |
+    * | Windows | `{FOLDERID_RoamingAppData}\_project_path_\data`                        | C:\Users\Alice\AppData\Roaming\Foo Corp\Bar App\data               |
+    * </code>
+    */
   public final String dataDir;
+  /** Returns the path to the project's local data directory.
+    * <p>
+    * <code style="white-space: pre">
+    * |Platform | Value                                                                  | Example                                                            |
+    * | ------- | ---------------------------------------------------------------------- | ------------------------------------------------------------------ |
+    * | Linux   | `$XDG_DATA_HOME/_project_path_` or `$HOME/.local/share/_project_path_` | /home/alice/.local/share/barapp                                    |
+    * | macOS   | `$HOME/Library/Application Support/_project_path_`                     | /Users/Alice/Library/Application Support/com.Foo-Corp.Bar-App/data |
+    * | Windows | `{FOLDERID_LocalAppData}\_project_path_\data`                          | C:\Users\Alice\AppData\Local\Foo Corp\Bar App\data                 |
+    * </code>
+    */
   public final String dataLocalDir;
+  /** Returns the path to the project's runtime directory.
+    * <p>
+    * <code style="white-space: pre">
+    * |Platform | Value              | Example               |
+    * | ------- | ------------------ | --------------------- |
+    * | Linux   | `$XDG_RUNTIME_DIR` | /run/user/1001/barapp |
+    * | macOS   | –                  | –                     |
+    * | Windows | –                  | –                     |
+    * </code>
+    */
   public final String runtimeDir;
 
+  /** Creates a <code>ProjectDirectories</code> instance directly from a path.
+    * <p>
+    * The argument is used verbatim and is not adapted to operating system standards.
+    * The use of {@link ProjectDirectories#fromPath} is strongly discouraged, as its results will
+    * not follow operating system standards on at least two of three platforms.
+    *
+    * @param path A string used verbatim as the path fragment to derive the directory field values.
+    * @return An instance of <code>ProjectDirectories</code>, whose directory field values are directly derived from the <code>path</code> argument.
+    */
   public static ProjectDirectories fromPath(String path) {
-    String path_ = path + '/';
     String homeDir;
     String cacheDir;
     String configDir;
@@ -42,68 +121,90 @@ public final class ProjectDirectories {
     switch (operatingSystem) {
       case LIN:
       case BSD:
-        homeDir      = System.getenv("HOME");
-        cacheDir     = defaultIfNullOrEmptyExtended(System.getenv("XDG_CACHE_HOME"),  '/' + path_, homeDir + "/.cache/",       path_);
-        configDir    = defaultIfNullOrEmptyExtended(System.getenv("XDG_CONFIG_HOME"), '/' + path_, homeDir + "/.config/",      path_);
-        dataDir      = defaultIfNullOrEmptyExtended(System.getenv("XDG_DATA_HOME"),   '/' + path_, homeDir + "/.local/share/", path_);
+        homeDir      = System.getProperty("user.home");
+        cacheDir     = defaultIfNullOrEmptyExtended(System.getenv("XDG_CACHE_HOME"),  '/' + path, homeDir + "/.cache/",       path);
+        configDir    = defaultIfNullOrEmptyExtended(System.getenv("XDG_CONFIG_HOME"), '/' + path, homeDir + "/.config/",      path);
+        dataDir      = defaultIfNullOrEmptyExtended(System.getenv("XDG_DATA_HOME"),   '/' + path, homeDir + "/.local/share/", path);
         dataLocalDir = dataDir;
-        runtimeDir   = linuxRuntimeDir(homeDir, path_);
+        runtimeDir   = linuxRuntimeDir(homeDir, path);
         break;
       case MAC:
-        homeDir      = System.getenv("HOME");
-        cacheDir     = homeDir + "/Library/Caches/"              + path_;
-        configDir    = homeDir + "/Library/Preferences/"         + path_;
-        dataDir      = homeDir + "/Library/Application Support/" + path_;
+        homeDir      = System.getProperty("user.home");
+        cacheDir     = homeDir + "/Library/Caches/"              + path;
+        configDir    = homeDir + "/Library/Preferences/"         + path;
+        dataDir      = homeDir + "/Library/Application Support/" + path;
         dataLocalDir = dataDir;
         break;
       case WIN:
-        dataDir      = runPowerShellCommand("ApplicationData")      + '/' + path_;
-        dataLocalDir = runPowerShellCommand("LocalApplicationData") + '/' + path_;
-        configDir    = dataDir;
-        cacheDir     = dataDir + "cache/";
+        String appData      = runPowerShellCommand("ApplicationData") + '/' + path;
+        String appDataLocal = runPowerShellCommand("LocalApplicationData") + '/' + path;
+        dataDir      = appData      + "/data";
+        dataLocalDir = appDataLocal + "/data";
+        configDir    = dataDir      + "/config";
+        cacheDir     = dataDir      + "/cache";
         break;
       default:
-        throw new UnsupportedOperatingSystemException("Base directories are not supported on " + operatingSystemName);
+        throw new UnsupportedOperatingSystemException("Project directories are not supported on " + operatingSystemName);
     }
-    return new ProjectDirectories(path_, cacheDir, configDir, dataDir, dataLocalDir, runtimeDir);
+    return new ProjectDirectories(path, cacheDir, configDir, dataDir, dataLocalDir, runtimeDir);
   }
 
-  public static ProjectDirectories from(String qualifier, String organization, String project) {
+  /** Creates a <code>ProjectDirectories</code> instance from values describing the project.
+    * <p>
+    * The use of {@link ProjectDirectories#from} – instead of {@link ProjectDirectories#fromPath} – is strongly encouraged,
+    * as its results will follow operating system standards on Linux, macOS and Windows.
+    *
+    * @param qualifier The reverse domain name notation of the application, excluding the
+    *         organization or application name itself.<br>
+    *         An empty string can be passed if no qualifier should be used (only affects macOS).<br>
+    *         Example values: <code>"com.example"</code>, <code>"org"</code>, <code>"uk.co"</code>, <code>"io"</code>, <code>""</code>
+    * @param organization The name of the organization that develops this application, or for which
+    *         the application is developed.<br>
+    *         An empty string can be passed if no organization should be used (only affects macOS
+    *         and Windows).<br>
+    *         Example values: <code>"Foo Corp"</code>, <code>"Alice and Bob Inc"</code>, <code>""</code>
+    * @param application
+    *         The name of the application itself.<br>
+    *         Example values: <code>"Bar App"</code>, <code>"ExampleProgram"</code>, <code>"Unicorn-Programme"</code>
+    * @return An instance of <code>ProjectDirectories</code>, whose directory field values are based on the
+    *         <code>qualifier</code>, <code>organization</code> and <code>application</code> arguments.
+    */
+  public static ProjectDirectories from(String qualifier, String organization, String application) {
     String path;
     StringBuilder buf;
     boolean orgPresent;
-    boolean projPresent;
+    boolean appPresent;
     switch (operatingSystem) {
       case LIN:
       case BSD:
-        path = trimLowercaseReplaceWhitespace(project, "");
+        path = trimLowercaseReplaceWhitespace(application, "");
         break;
       case MAC:
-        buf = new StringBuilder(Math.max(stringLength(qualifier) + stringLength(organization) + stringLength(project), 0));
+        buf = new StringBuilder(Math.max(stringLength(qualifier) + stringLength(organization) + stringLength(application), 0));
         boolean qualPresent = !isNullOrEmpty(qualifier);
                 orgPresent  = !isNullOrEmpty(organization);
-                projPresent = !isNullOrEmpty(project);
+                appPresent = !isNullOrEmpty(application);
         if (qualPresent)
           buf.append(qualifier.replace(' ', '-'));
-          if (orgPresent || projPresent)
+          if (orgPresent || appPresent)
             buf.append('.');
         if (orgPresent)
           buf.append(organization.replace(' ', '-'));
-          if (projPresent)
+          if (appPresent)
             buf.append('.');
-        if (!isNullOrEmpty(project))
-          buf.append(project.replace(' ', '-'));
+        if (!isNullOrEmpty(application))
+          buf.append(application.replace(' ', '-'));
         path = buf.toString();
       case WIN:
-        buf = new StringBuilder(Math.max(stringLength(organization) + stringLength(project), 0));
+        buf = new StringBuilder(Math.max(stringLength(organization) + stringLength(application), 0));
         orgPresent  = !isNullOrEmpty(organization);
-        projPresent = !isNullOrEmpty(project);
+        appPresent = !isNullOrEmpty(application);
         if (orgPresent)
           buf.append(organization);
-          if (projPresent)
+          if (appPresent)
             buf.append('/');
-        if (projPresent)
-          buf.append(project);
+        if (appPresent)
+          buf.append(application);
         path = buf.toString();
         break;
       default:
---
+++ src/main/java/io/github/soc/directories/UserDirectories.java
@@ -0,0 +1,182 @@
+package io.github.soc.directories;
+
+import static io.github.soc.directories.Util.*;
+
+/** <code>UserDirectories</code> provides paths of user-facing standard directories, following the conventions of the operating system the library is running on.
+  * <p>
+  * It is a snapshot of the state of the system at the time this class is initialized.
+  *
+  * <h2>Examples</h2>
+  *
+  * All examples on this page are computed with a user named <em>Alice</em>.
+  * <p>
+  * <code style="white-space: pre">
+  * UserDirectories.audioDir;
+  * // Linux:   /home/alice/Music
+  * // Windows: /Users/Alice/Music
+  * // macOS:   C:\Users\Alice\Music
+  * </code>
+  */
+public final class UserDirectories {
+
+  private UserDirectories() {
+    throw new Error();
+  }
+  /** Returns the path to the user's home directory.
+    * <p>
+    * <code style="white-space: pre">
+    * |Platform | Value                | Example        |
+    * | ------- | -------------------- | -------------- |
+    * | Linux   | `$HOME`              | /home/alice    |
+    * | macOS   | `$HOME`              | /Users/Alice   |
+    * | Windows | `{FOLDERID_Profile}` | C:\Users\Alice |
+    * </code>
+    */
+  public static final String homeDir;
+
+  // user directories
+
+  /** Returns the path to the user's audio directory.
+    * <p>
+    * <code style="white-space: pre">
+    * |Platform | Value              | Example              |
+    * | ------- | ------------------ | -------------------- |
+    * | Linux   | `XDG_MUSIC_DIR`    | /home/alice/Music    |
+    * | macOS   | `$HOME/Music`      | /Users/Alice/Music   |
+    * | Windows | `{FOLDERID_Music}` | C:\Users\Alice\Music |
+    * </code>
+    */
+  public static final String audioDir;
+  /** Returns the path to the user's desktop directory.
+    * <p>
+    * <code style="white-space: pre">
+    * |Platform | Value                | Example                |
+    * | ------- | -------------------- | ---------------------- |
+    * | Linux   | `XDG_DESKTOP_DIR`    | /home/alice/Desktop    |
+    * | macOS   | `$HOME/Desktop`      | /Users/Alice/Desktop   |
+    * | Windows | `{FOLDERID_Desktop}` | C:\Users\Alice\Desktop |
+    * </code>
+    */
+  public static final String desktopDir;
+  /** Returns the path to the user's document directory.
+    * <p>
+    * <code style="white-space: pre">
+    * |Platform | Value                  | Example                  |
+    * | ------- | ---------------------- | ------------------------ |
+    * | Linux   | `XDG_DOCUMENTS_DIR`    | /home/alice/Documents    |
+    * | macOS   | `$HOME/Documents`      | /Users/Alice/Documents   |
+    * | Windows | `{FOLDERID_Documents}` | C:\Users\Alice\Documents |
+    * </code>
+    */
+  public static final String documentDir;
+  /** Returns the path to the user's download directory.
+    * <p>
+    * <code style="white-space: pre">
+    * |Platform | Value                  | Example                  |
+    * | ------- | ---------------------- | ------------------------ |
+    * | Linux   | `XDG_DOWNLOAD_DIR`     | /home/alice/Downloads    |
+    * | macOS   | `$HOME/Downloads`      | /Users/Alice/Downloads   |
+    * | Windows | `{FOLDERID_Downloads}` | C:\Users\Alice\Downloads |
+    * </code>
+    */
+  public static final String downloadDir;
+  /** Returns the path to the user's font directory.
+    * <p>
+    * <code style="white-space: pre">
+    * |Platform | Value                                                | Example                        |
+    * | ------- | ---------------------------------------------------- | ------------------------------ |
+    * | Linux   | `$XDG_DATA_HOME/fonts` or `$HOME/.local/share/fonts` | /home/alice/.local/share/fonts |
+    * | macOS   | `$HOME/Library/Fonts`                                | /Users/Alice/Library/Fonts     |
+    * | Windows | null                                                 | null                           |
+    * </code>
+    */
+  public static final String fontDir;
+  /** Returns the path to the user's picture directory.
+    * <p>
+    * <code style="white-space: pre">
+    * |Platform | Value                 | Example                 |
+    * | ------- | --------------------- | ----------------------- |
+    * | Linux   | `XDG_PICTURES_DIR`    | /home/alice/Pictures    |
+    * | macOS   | `$HOME/Pictures`      | /Users/Alice/Pictures   |
+    * | Windows | `{FOLDERID_Pictures}` | C:\Users\Alice\Pictures |
+    * </code>
+    */
+  public static final String pictureDir;
+  /** Returns the path to the user's public directory.
+    * <p>
+    * <code style="white-space: pre">
+    * |Platform | Value                 | Example             |
+    * | ------- | --------------------- | ------------------- |
+    * | Linux   | `XDG_PUBLICSHARE_DIR` | /home/alice/Public  |
+    * | macOS   | `$HOME/Public`        | /Users/Alice/Public |
+    * | Windows | `{FOLDERID_Public}`   | C:\Users\Public     |
+    * </code>
+    */
+  public static final String publicDir;
+  /** Returns the path to the user's template directory.
+    * <p>
+    * <code style="white-space: pre">
+    * |Platform | Value                  | Example                                                    |
+    * | ------- | ---------------------- | ---------------------------------------------------------- |
+    * | Linux   | `XDG_TEMPLATES_DIR`    | /home/alice/Templates                                      |
+    * | macOS   | null                   | null                                                       |
+    * | Windows | `{FOLDERID_Templates}` | C:\Users\Alice\AppData\Roaming\Microsoft\Windows\Templates |
+    * </code>
+    */
+  public static final String templateDir;
+  /** Returns the path to the user's video directory.
+    * <p>
+    * <code style="white-space: pre">
+    * |Platform | Value               | Example               |
+    * | ------- | ------------------- | --------------------- |
+    * | Linux   | `XDG_VIDEOS_DIR`    | /home/alice/Videos    |
+    * | macOS   | `$HOME/Movies`      | /Users/Alice/Movies   |
+    * | Windows | `{FOLDERID_Videos}` | C:\Users\Alice\Videos |
+    * </code>
+    */
+  public static final String videoDir;
+
+  static {
+    switch (operatingSystem) {
+      case LIN:
+      case BSD:
+        homeDir       = System.getProperty("user.home");
+        audioDir      = runXDGUserDir("MUSIC");
+        desktopDir    = runXDGUserDir("DESKTOP");
+        documentDir   = runXDGUserDir("DOCUMENTS");
+        downloadDir   = runXDGUserDir("DOWNLOAD");
+        fontDir       = defaultIfNullOrEmptyExtended(System.getenv("XDG_DATA_HOME"), "/fonts/",  homeDir, "/.local/share/fonts/");
+        pictureDir    = runXDGUserDir("PICTURES");
+        publicDir     = runXDGUserDir("PUBLICSHARE");
+        templateDir   = runXDGUserDir("TEMPLATES");
+        videoDir      = runXDGUserDir("VIDEOS");
+        break;
+      case MAC:
+        homeDir       = System.getProperty("user.home");
+        audioDir      = homeDir + "/Music/";
+        desktopDir    = homeDir + "/Desktop/";
+        documentDir   = homeDir + "/Documents/";
+        downloadDir   = homeDir + "/Downloads/";
+        fontDir       = homeDir + "/Library/Fonts/";
+        pictureDir    = homeDir + "/Pictures/";
+        publicDir     = homeDir + "/Public/";
+        templateDir   = null;
+        videoDir      = homeDir + "/Movies/";
+        break;
+      case WIN:
+        homeDir       = runPowerShellCommand("UserProfile");
+        audioDir      = runPowerShellCommand("Music");
+        fontDir       = null;
+        desktopDir    = runPowerShellCommand("Desktop");
+        documentDir   = runPowerShellCommand("Documents");
+        downloadDir   = runPowerShellCommand("Downloads");
+        pictureDir    = runPowerShellCommand("Pictures");
+        publicDir     = runPowerShellCommand("Public");
+        templateDir   = runPowerShellCommand("Templates");
+        videoDir      = runPowerShellCommand("Videos");
+        break;
+      default:
+        throw new UnsupportedOperatingSystemException("User directories are not supported on " + operatingSystemName);
+    }
+  }
+}
