072bef287b0b75a9e49c7f64b40a10e6d2edf908
SackCastellon <SackCastellon@users.noreply.github.com>
2018-03-21 19:34:23 +0100

    Improved Javadoc and some tests
    - Added HTML tables Javadoc and improved some documentation
    - Changed some variable names in the tests
    - Split one test into two
    - Added missing "Test" annotation to some tests
    - Reformated code

--- src/main/java/io/github/soc/directories/BaseDirectories.java
+++ src/main/java/io/github/soc/directories/BaseDirectories.java
@@ -2,22 +2,22 @@ package io.github.soc.directories;
 
 import static io.github.soc.directories.Util.*;
 
-/** <code>BaseDirectories</code> provides paths of user-invisible standard directories, following the conventions of the operating system the library is running on.
+/** {@code BaseDirectories} provides paths of user-invisible standard directories, following the conventions of the operating system the library is running on.
   * <p>
   * It is a snapshot of the state of the system at the time this class is initialized.
   * <p>
   * To compute the location of cache, config or data directories for individual projects or applications, use {@link ProjectDirectories} instead.
   *
   * <h2>Examples</h2>
-  *
+  * <p>
   * All examples on this page are computed with a user named <em>Alice</em>.
   * <p>
-  * <code style="white-space: pre">
-  * BaseDirs.configDir
-  * // Linux:   /home/alice/.config
-  * // Windows: C:\Users\Alice\AppData\Roaming
-  * // macOS:   /Users/Alice/Library/Preferences
-  * </code>
+  * Example of {@link ProjectDirectories#configDir} value in different operating systems:
+  * <ul>
+  * <li><b>Linux/BSD:</b> {@code /home/alice/.config}</li>
+  * <li><b>macOS:</b> {@code /Users/Alice/Library/Preferences}</li>
+  * <li><b>Windows:</b> {@code C:\Users\Alice\AppData\Roaming}</li>
+  * </ul>
   */
 public final class BaseDirectories {
 
@@ -26,83 +26,191 @@ public final class BaseDirectories {
   }
 
   /** Returns the path to the user's home directory.
-    * <p>
-    * <code style="white-space: pre">
-    * |Platform | Value                | Example         |
-    * | ------- | -------------------- | --------------- |
-    * | Linux   | `$HOME`              | /home/alice     |
-    * | macOS   | `$HOME`              | /Users/Alice    |
-    * | Windows | `{FOLDERID_Profile}` | C:\Users\Alice\ |
-    * </code>
+    * <br><br>
+    * <table border="1" cellpadding="1" cellspacing="0">
+    * <tr>
+    * <th align="left">Platform</th>
+    * <th align="left">Value</th>
+    * <th align="left">Example</th>
+    * </tr>
+    * <tr>
+    * <td>Linux/BSD</td>
+    * <td>{@code $HOME}</td>
+    * <td>/home/alice</td>
+    * </tr>
+    * <tr>
+    * <td>macOS</td>
+    * <td>{@code $HOME}</td>
+    * <td>/Users/Alice</td>
+    * </tr>
+    * <tr>
+    * <td>Windows</td>
+    * <td>{@code {FOLDERID_Profile}}</td>
+    * <td>C:\Users\Alice\</td>
+    * </tr>
+    * </table>
     */
   public static final String homeDir;
 
-  // base directories
-
   /** Returns the path to the user's cache directory.
-    * <p>
-    * <code style="white-space: pre">
-    * |Platform | Value                               | Example                      |
-    * | ------- | ----------------------------------- | ---------------------------- |
-    * | Linux   | `$XDG_CACHE_HOME` or `$HOME`/.cache | /home/alice/.cache           |
-    * | macOS   | `$HOME`/Library/Caches              | /Users/Alice/Library/Caches  |
-    * | Windows | `{FOLDERID_LocalAppData}`\cache     | C:\Users\Alice\AppData\Local |
-    * </code>
+    * <br><br>
+    * <table border="1" cellpadding="1" cellspacing="0">
+    * <tr>
+    * <th align="left">Platform</th>
+    * <th align="left">Value</th>
+    * <th align="left">Example</th>
+    * </tr>
+    * <tr>
+    * <td>Linux/BSD</td>
+    * <td>{@code $XDG_CACHE_HOME} or {@code $HOME}/.cache</td>
+    * <td>/home/alice/.cache</td>
+    * </tr>
+    * <tr>
+    * <td>macOS</td>
+    * <td>{@code $HOME}/Library/Caches</td>
+    * <td>/Users/Alice/Library/Caches</td>
+    * </tr>
+    * <tr>
+    * <td>Windows</td>
+    * <td>{@code {FOLDERID_LocalAppData}}\cache</td>
+    * <td>C:\Users\Alice\AppData\Local</td>
+    * </tr>
+    * </table>
     */
   public static final String cacheDir;
+
   /** Returns the path to the user's config directory.
-    * <p>
-    * <code style="white-space: pre">
-    * |Platform | Value                                 | Example                          |
-    * | ------- | ------------------------------------- | -------------------------------- |
-    * | Linux   | `$XDG_CONFIG_HOME` or `$HOME`/.config | /home/alice/.config              |
-    * | macOS   | `$HOME`/Library/Preferences           | /Users/Alice/Library/Preferences |
-    * | Windows | `{FOLDERID_RoamingAppData}`           | C:\Users\Alice\AppData\Roaming   |
-    * </code>
+    * <br><br>
+    * <table border="1" cellpadding="1" cellspacing="0">
+    * <tr>
+    * <th align="left">Platform</th>
+    * <th align="left">Value</th>
+    * <th align="left">Example</th>
+    * </tr>
+    * <tr>
+    * <td>Linux/BSD</td>
+    * <td>{@code $XDG_CONFIG_HOME} or {@code $HOME}/.config</td>
+    * <td>/home/alice/.config</td>
+    * </tr>
+    * <tr>
+    * <td>macOS</td>
+    * <td>{@code $HOME}/Library/Preferences</td>
+    * <td>/Users/Alice/Library/Preferences</td>
+    * </tr>
+    * <tr>
+    * <td>Windows</td>
+    * <td>{@code {FOLDERID_RoamingAppData}}</td>
+    * <td>C:\Users\Alice\AppData\Roaming</td>
+    * </tr>
+    * </table>
     */
   public static final String configDir;
+
   /** Returns the path to the user's data directory.
-    * <p>
-    * <code style="white-space: pre">
-    * |Platform | Value                                    | Example                                  |
-    * | ------- | ---------------------------------------- | ---------------------------------------- |
-    * | Linux   | `$XDG_DATA_HOME` or `$HOME`/.local/share | /home/alice/.local/share                 |
-    * | macOS   | `$HOME`/Library/Application Support      | /Users/Alice/Library/Application Support |
-    * | Windows | `{FOLDERID_RoamingAppData}`              | C:\Users\Alice\AppData\Roaming           |
-    * </code>
+    * <br><br>
+    * <table border="1" cellpadding="1" cellspacing="0">
+    * <tr>
+    * <th align="left">Platform</th>
+    * <th align="left">Value</th>
+    * <th align="left">Example</th>
+    * </tr>
+    * <tr>
+    * <td>Linux/BSD</td>
+    * <td>{@code $XDG_DATA_HOME} or {@code $HOME}/.local/share</td>
+    * <td>/home/alice/.local/share</td>
+    * </tr>
+    * <tr>
+    * <td>macOS</td>
+    * <td>{@code $HOME}/Library/Application Support</td>
+    * <td>/Users/Alice/Library/Application Support</td>
+    * </tr>
+    * <tr>
+    * <td>Windows</td>
+    * <td>{@code {FOLDERID_RoamingAppData}}</td>
+    * <td>C:\Users\Alice\AppData\Roaming</td>
+    * </tr>
+    * </table>
     */
   public static final String dataDir;
+
   /** Returns the path to the user's local data directory.
-    * <p>
-    * <code style="white-space: pre">
-    * |Platform | Value                                    | Example                                  |
-    * | ------- | ---------------------------------------- | ---------------------------------------- |
-    * | Linux   | `$XDG_DATA_HOME` or `$HOME`/.local/share | /home/alice/.local/share                 |
-    * | macOS   | `$HOME`/Library/Application Support      | /Users/Alice/Library/Application Support |
-    * | Windows | `{FOLDERID_LocalAppData}`                | C:\Users\Alice\AppData\Local             |
-    * </code>
+    * <br><br>
+    * <table border="1" cellpadding="1" cellspacing="0">
+    * <tr>
+    * <th align="left">Platform</th>
+    * <th align="left">Value</th>
+    * <th align="left">Example</th>
+    * </tr>
+    * <tr>
+    * <td>Linux/BSD</td>
+    * <td>{@code $XDG_DATA_HOME} or {@code $HOME}/.local/share</td>
+    * <td>/home/alice/.local/share</td>
+    * </tr>
+    * <tr>
+    * <td>macOS</td>
+    * <td>{@code $HOME}/Library/Application Support</td>
+    * <td>/Users/Alice/Library/Application Support</td>
+    * </tr>
+    * <tr>
+    * <td>Windows</td>
+    * <td>{@code {FOLDERID_LocalAppData}}</td>
+    * <td>C:\Users\Alice\AppData\Local</td>
+    * </tr>
+    * </table>
     */
   public static final String dataLocalDir;
+
   /** Returns the path to the user's executable directory.
-    * <p>
-    * <code style="white-space: pre">
-    * |Platform | Value                                                            | Example                |
-    * | ------- | ---------------------------------------------------------------- | ---------------------- |
-    * | Linux   | `$XDG_BIN_HOME` or `$XDG_DATA_HOME`/../bin or `$HOME`/.local/bin | /home/alice/.local/bin |
-    * | macOS   | –                                                                | null                   |
-    * | Windows | –                                                                | null                   |
-    * </code>
+    * <br><br>
+    * <table border="1" cellpadding="1" cellspacing="0">
+    * <tr>
+    * <th align="left">Platform</th>
+    * <th align="left">Value</th>
+    * <th align="left">Example</th>
+    * </tr>
+    * <tr>
+    * <td>Linux/BSD</td>
+    * <td>{@code $XDG_BIN_HOME} or {@code $XDG_DATA_HOME}/../bin or {@code $HOME}/.local/bin</td>
+    * <td>/home/alice/.local/bin</td>
+    * </tr>
+    * <tr>
+    * <td>macOS</td>
+    * <td>–</td>
+    * <td>{@code null}</td>
+    * </tr>
+    * <tr>
+    * <td>Windows</td>
+    * <td>–</td>
+    * <td>{@code null}</td>
+    * </tr>
+    * </table>
     */
   public static final String executableDir;
+
   /** Returns the path to the user's runtime directory.
-    * <p>
-    * <code style="white-space: pre">
-    * |Platform | Value              | Example         |
-    * | ------- | ------------------ | --------------- |
-    * | Linux   | `$XDG_RUNTIME_DIR` | /run/user/1001/ |
-    * | macOS   | –                  | null            |
-    * | Windows | –                  | null            |
-    * </code>
+    * <br><br>
+    * <table border="1" cellpadding="1" cellspacing="0">
+    * <tr>
+    * <th align="left">Platform</th>
+    * <th align="left">Value</th>
+    * <th align="left">Example</th>
+    * </tr>
+    * <tr>
+    * <td>Linux/BSD</td>
+    * <td>{@code $XDG_RUNTIME_DIR}</td>
+    * <td>/run/user/1001/</td>
+    * </tr>
+    * <tr>
+    * <td>macOS</td>
+    * <td>–</td>
+    * <td>{@code null}</td>
+    * </tr>
+    * <tr>
+    * <td>Windows</td>
+    * <td>–</td>
+    * <td>{@code null}</td>
+    * </tr>
+    * </table>
     */
   public static final String runtimeDir;
 
--- src/test/java/io/github/soc/directories/UtilTest.java
+++ src/test/java/io/github/soc/directories/UtilTest.java
@@ -1,47 +1,56 @@
 package io.github.soc.directories;
 
-import        org.junit.Test;
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public final class UtilTest {
-  @Test
-  public void testStripQualification() {
-    String actual1   = Util.stripQualification("org.foo.BarApp");
-    String expected1 = "BarApp";
-    assertEquals(actual1, expected1);
 
-    String actual2   = Util.stripQualification("BarApp");
-    String expected2 = "BarApp";
-    assertEquals(actual2, expected2);
+  @Test
+  public void testStripQualification01() {
+    final String input    = "org.foo.BarApp";
+    final String actual   = Util.stripQualification(input);
+    final String expected = "BarApp";
+    assertEquals(input, expected, actual);
   }
 
   @Test
-
-  public void testTrimLowercaseAndReplaceWhitespaces1() {
-    String input1    = "Bar App";
-    String actual1   = Util.trimLowercaseReplaceWhitespace(input1, "-");
-    String expected1 = "bar-app";
-    assertEquals(input1,  expected1, actual1);
+  public void testStripQualification02() {
+    final String input    = "BarApp";
+    final String actual   = Util.stripQualification(input);
+    final String expected = "BarApp";
+    assertEquals(input, expected, actual);
   }
 
-  public void testTrimLowercaseAndReplaceWhitespaces2() {
-    String input2    = "BarApp Foo";
-    String actual2   = Util.trimLowercaseReplaceWhitespace(input2, "");
-    String expected2 = "barappfoo";
-    assertEquals(input2, expected2, actual2);
+  @Test
+  public void testTrimLowercaseAndReplaceWhitespaces01() {
+    final String input    = "Bar App";
+    final String actual   = Util.trimLowercaseReplaceWhitespace(input, "-");
+    final String expected = "bar-app";
+    assertEquals(input, expected, actual);
   }
 
-  public void testTrimLowercaseAndReplaceWhitespaces3() {
-    String input3    = " Bar App ";
-    String actual3   = Util.trimLowercaseReplaceWhitespace(input3, "-");
-    String expected3 = "bar-app";
-    assertEquals(input3, expected3, actual3);
+  @Test
+  public void testTrimLowercaseAndReplaceWhitespaces02() {
+    final String input    = "BarApp Foo";
+    final String actual   = Util.trimLowercaseReplaceWhitespace(input, "");
+    final String expected = "barappfoo";
+    assertEquals(input, expected, actual);
   }
 
-  public void testTrimLowercaseAndReplaceWhitespaces4() {
-    String input4    = "  Bar  App  ";
-    String actual4   = Util.trimLowercaseReplaceWhitespace(input4, "-");
-    String expected4 = "bar-app";
-    assertEquals(input4, expected4, actual4);
+  @Test
+  public void testTrimLowercaseAndReplaceWhitespaces03() {
+    final String input    = " Bar App ";
+    final String actual   = Util.trimLowercaseReplaceWhitespace(input, "-");
+    final String expected = "bar-app";
+    assertEquals(input, expected, actual);
+  }
+
+  @Test
+  public void testTrimLowercaseAndReplaceWhitespaces04() {
+    final String input    = "  Bar  App  ";
+    final String actual   = Util.trimLowercaseReplaceWhitespace(input, "-");
+    final String expected = "bar-app";
+    assertEquals(input, expected, actual);
   }
 }
